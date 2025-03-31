264bbae2b0fef8b8804ded0b23b842662adf41be
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-03-31 16:16:29 +0200

    biography looks alright

--- components/biography.tsx
+++ components/biography.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+"use client";
+
+import { FaLinkedin } from "react-icons/fa6";
+import { TbFileCv } from "react-icons/tb";
+import { Magnetic } from "./motion-primitives/magnetic";
+import { MdEmail } from "react-icons/md";
 import { TextEffect } from "./motion-primitives/text-effect";
-importe
+import { motion } from "framer-motion";
+
+const MotionUL = motion.ul;
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  hidden: { opacity: 0, transition: { when: "afterChildren" } },
+};
+
+const item = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -20 },
+};
 
 function Biography() {
   return (
-    <div>
+    <div className="flex flex-col">
       <TextEffect
         preset="fade-in-blur"
         as="h1"
@@ -18,10 +41,46 @@ function Biography() {
         preset="fade-in-blur"
         as="p"
         className="text-muted-foreground"
+        speedReveal={3}
       >
         with a burning passion for programming, learning new skills and
         technologies and providing solutions.
       </TextEffect>
+
+      <MotionUL
+        className="flex w-full justify-center gap-12 mt-6"
+        initial="hidden"
+        animate="visible"
+        variants={list}
+      >
+        <motion.li variants={item}>
+          <Magnetic>
+            <a
+              href="https://www.linkedin.com/in/housein-abo-shaar-920292265/"
+              className="p-3"
+            >
+              <FaLinkedin size={36} />
+            </a>
+          </Magnetic>
+        </motion.li>
+        <motion.li variants={item}>
+          <Magnetic>
+            <a href="mailto:housein.aboshaar@gmail.com" className="p-3">
+              <MdEmail size={36} />
+            </a>
+          </Magnetic>
+        </motion.li>
+        <motion.li variants={item}>
+          <Magnetic>
+            <a
+              href="https://docs.houseinaboshaar.com/HousienAboShaar-Resume.pdf"
+              className="p-3"
+            >
+              <TbFileCv size={36} />
+            </a>
+          </Magnetic>
+        </motion.li>
+      </MotionUL>
     </div>
   );
 }
