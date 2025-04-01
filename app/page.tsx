ceed87329a0ae939b194f1ae4f8968d14ab57ed6
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-01 07:23:57 +0200

    added spotlight, looks cool.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import { ModeToggle } from "@/components/light-dark-toggle";
 import { Separator } from "@/components/ui/separator";
 import WorkExperience from "@/components/workexperience";
 import TechStack from "@/components/techstack";
+import { Spotlight } from "@/components/motion-primitives/spotlight";
 
 export default function Page() {
   return (
     <div className="container">
+      <Spotlight size={120} className="bg-sky-500/10 dark:bg-foreground/10" />
       <Biography />
       <About />
       <WorkExperience />
