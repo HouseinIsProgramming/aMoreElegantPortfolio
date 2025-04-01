f26b03672f7cd26d831dd4408657df0fa166ba30
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-01 08:12:49 +0200

    spotlight border added

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,16 @@ import { Spotlight } from "@/components/motion-primitives/spotlight";
 
 export default function Page() {
   return (
-    <div className="container">
-      <Spotlight size={120} className="bg-sky-500/10 dark:bg-foreground/10" />
-      <Biography />
-      <About />
-      <WorkExperience />
-      <Separator className="my-4" />
-      <TechStack />
-      <ModeToggle />
+    <div>
+      <Spotlight size={90} className="bg-gray-700/15 dark:bg-foreground/10" />
+      <div className="container">
+        <Biography />
+        <About />
+        <WorkExperience />
+        <Separator className="my-4" />
+        <TechStack />
+        <ModeToggle />
+      </div>
     </div>
   );
 }
--- components/workexperience.tsx
+++ components/workexperience.tsx
@@ -10,6 +10,7 @@ import {
   badgeVariants,
   sectionHeaderVariants,
 } from "@/utils/motion-variants";
+import { Spotlight } from "./motion-primitives/spotlight";
 
 // Create motion components from regular components
 const MotionCard = motion.create(Card);
@@ -91,9 +92,15 @@ export default function WorkExperience() {
           {workExperiences.map((experience) => (
             <MotionCard
               key={experience.id}
-              className="overflow-hidden"
+              className="overflow-hidden relative"
               variants={itemVariants}
             >
+              <Spotlight
+                className="bg-conic-180 from-red-600 via-purple-600 to-blue-600 -z-50"
+                size={200}
+              />
+              <div className="absolute inset-[0.3%] h-[99.4%] -z-10 w-[99.4%] rounded-lg bg-white dark:bg-black"></div>
+
               <CardContent className="p-6">
                 <div className="grid gap-6 md:grid-cols-[2fr_3fr]">
                   <div>
