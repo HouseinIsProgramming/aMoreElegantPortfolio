4dc4a44f6ba59e623e0398bdc5837fe41bf988d8
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-01 19:01:46 +0200

    fix smth

--- components/biography.tsx
+++ components/biography.tsx
@@ -13,7 +13,7 @@ const MotionUL = motion.ul;
 
 function Biography() {
   return (
-    <div className="flex flex-col border-r">
+    <div className="flex flex-col lg:border-r">
       <TextEffect
         preset="fade-in-blur"
         as="h1"
