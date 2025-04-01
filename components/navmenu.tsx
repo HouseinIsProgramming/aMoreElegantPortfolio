6e2e0430ec7942227a5e918a0283e4409ef91340
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-01 21:06:00 +0200

    attemptting..

--- components/navmenu.tsx
+++ components/navmenu.tsx
@@ -1,6 +1,7 @@
-import { AnimatedBackground } from "./motion-primitives/animated-background";
 import { motion } from "framer-motion";
 import { itemVariants } from "@/utils/motion-variants";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const TABS = [
   { title: "About", link: "about" },
@@ -10,18 +11,16 @@ const TABS = [
 ];
 
 function NavMenu() {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (item) => {
+    setHoveredItem(item);
+  };
+
   return (
-    <nav className="max-sm:mx-auto py-4 flex flex-col gap-3">
-      <AnimatedBackground
-        className="rounded-lg dark:bg-zinc-100 max-w-1/2 bg-zinc-800"
-        transition={{
-          type: "spring",
-          bounce: 0.2,
-          duration: 0.3,
-        }}
-        enableHover
-      >
-        {TABS.map((tab, index) => (
+    <motion.nav layout className="max-sm:mx-auto py-4 flex flex-col gap-3">
+      {TABS.map((tab, index) => (
+        <>
           <motion.button
             key={index}
             variants={itemVariants}
@@ -29,8 +28,10 @@ function NavMenu() {
             animate="visible"
             transition={{ delay: index * 0.1 }}
             data-id={tab.link}
+            onHoverStart={() => handleMouseEnter(tab.link)}
+            onHoverEnd={() => setHoveredItem(null)}
             type="button"
-            className="px-2 py-0.5 text-zinc-600 transition-colors duration-300 hover:dark:text-zinc-950 dark:text-zinc-400 hover:text-zinc-50"
+            className="px-2 py-0.5 relative text-zinc-600 max-w-min whitespace-nowrap transition-colors duration-300 hover:dark:text-zinc-950 dark:text-zinc-400 hover:text-zinc-50"
             onClick={() => {
               const element = document.getElementById(tab.link);
               if (element) {
@@ -42,10 +43,18 @@ function NavMenu() {
             }}
           >
             {tab.title}
+            <AnimatePresence>
+              {hoveredItem == tab.link && (
+                <motion.div
+                  layoutId="bg"
+                  className="absolute w-full rounded-lg -z-10 bg-black h-full inset-0"
+                />
+              )}
+            </AnimatePresence>
           </motion.button>
-        ))}
-      </AnimatedBackground>
-    </nav>
+        </>
+      ))}
+    </motion.nav>
   );
 }
 
