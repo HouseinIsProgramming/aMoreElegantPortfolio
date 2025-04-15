4e336201115912bfabdbbde1bdc8448292033c65
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-15 17:06:24 +0200

    most important thing on any link

--- components/mini-footer.tsx
+++ components/mini-footer.tsx
@@ -5,6 +5,7 @@ export default function MiniFooter() {
     <footer className="!text-center text-muted-foreground">
       <a
         href="https://github.com/HouseinIsProgramming/aMoreElegantPortfolio"
+        target="_blank"
         className="flex flex-col items-center gap-1"
       >
         <span className="hover:bg-muted-foreground/10 rounded-md px-2 py-1 transition-colors duration-500 ease-in-out">
